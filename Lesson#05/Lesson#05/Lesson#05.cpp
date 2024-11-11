2cc37401eb7ab3989138ebda6961a376c648dd42
Soviskas <kisilovoleksandr48@gmail.com>
2024-11-11 12:39:46 +0200

    Add files via upload

---
+++ Lesson#05/Lesson#05/Lesson#05.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int mainNuber = 6789;
+
+	cout << "Original number: " << mainNuber << endl;
+	cout << "Reworking number: ";
+	cout << mainNuber % 10;
+	mainNuber /= 10;
+	cout << mainNuber % 10;
+	mainNuber /= 10;
+	cout << mainNuber % 10;
+	mainNuber /= 10;
+	cout << mainNuber % 10;
+	mainNuber /= 10;
+
+	cout << endl << endl;
+
+	return 0;
+}
